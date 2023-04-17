030b80e10aa1acf47507cbf5a8ec43d058d2cd0d
Rikardo Akosta <richy.andres.acosta@gmail.com>
2023-04-18 01:17:51 +0200

    se crea y se prueban rutas de usuario en postman

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import express from "express";
 import autRouter from "./routes/auth.routes.js";
 
 const app = express();
-
+app.use(express.json());
 app.use("/api/v1", autRouter);
 
 const PORT = process.env.PORT || 6000;
